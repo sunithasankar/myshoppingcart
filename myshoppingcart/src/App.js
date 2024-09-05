07055e726088c22e426673f9af653f562f59a678
Naga Maruti Sankar <v.sunithasankar@gmail.com>
2024-09-05 15:53:34 +0530

    This is Footer

--- myshoppingcart/src/App.js
+++ myshoppingcart/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
-
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 function App() {
   return (
-  <h1>This Is React Project</h1>   
+    <>
+  <h1>This Is React Project</h1> 
+  <Header/> 
+  <Footer/>
+  </>
   );
 }
 
---
+++ myshoppingcart/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <h4>This is from Footer</h4>
+    </div>
+  )
+}
+
+export default Footer
+
